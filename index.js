8e01aa1a03cd7c51bd930c8098cf3d0ae8580267
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-11-17 21:34:26 +0900

    Middlewares

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hi');
+import express from "express";
+const app = express()
+ 
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log(`Listening on: ${PORT}`);
+
+
+const handleHome = (req, res) => {res.send("Hi from home!!!");}
+
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("between");
+    next();
+};
+
+
+app.get("/", handleHome);
+
+app.use(betweenHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
