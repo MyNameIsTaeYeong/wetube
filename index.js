88ce24391b01ec077f4c7812c90e6a3cd67e2319
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-11-19 09:22:08 +0900

    Middlewares part Two
    1.
    
    morgan : logging,
    helmet : security,
    cookie-parser : for using cookie,
    body-parser : to access request Object.
    
    2. middleware can kill the connection.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import morgan from "morgan"; 
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
 const app = express()
  
 const PORT = 4000;
@@ -12,15 +18,14 @@ const handleHome = (req, res) => {res.send("Hi from home!!!");}
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("between");
-    next();
-};
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
-app.use(betweenHome);
 
 app.get("/profile", handleProfile);
 
