c6efa02ee8a7ff2752048697e23a30f0ee2d840f
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-11-19 09:50:53 +0900

    Routing

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
-
-const app = express()
- 
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on: ${PORT}`);
-
+const app = express();
 
 const handleHome = (req, res) => {res.send("Hi from home!!!");}
-
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
@@ -29,4 +22,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
