9e4f5cb7ef67eab95aaf88933bb14b15d37a959f
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-11-28 11:18:28 +0900

    Configuring Dot Env
    중요한 정보를 .env파일 내부에 저장하고
    .gitignore에 .env를 추가한다.

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/we-tube", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 
