2d25de0cb524e380c163675348a9705a9dccd09c
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-12-19 16:13:37 +0900

    MongoStore and Middlewares

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,21 +1,22 @@
 import express from "express";
 import { postJoin, getJoin, getLogin, logout, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { onlyPublic } from "../middlewares";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPublic, logout);
 
 
 export default globalRouter;
