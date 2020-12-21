88eab494d73de0280fafe9ec322ab4f22037ae4c
MyNameIsTaeYeong <imtaebari@gmail.com>
2020-12-21 11:06:54 +0900

    Github Log In part Three

--- passport.js
+++ passport.js
@@ -14,5 +14,10 @@ passport.use(new GithubStrategy({
 githubLoginCallback
 ));
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done){
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done){
+    done(null, user);
+});
